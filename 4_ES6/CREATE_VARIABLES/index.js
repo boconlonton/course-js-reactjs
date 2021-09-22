743e4c186d39db2ab60dee3eb26afc80876e15d3
bocon <tan.truong@go2joy.vn>
2021-09-22 22:09:41 +0700

    Add - ES6 - Create variables, function

---
+++ 4_ES6/CREATE_VARIABLES/index.js
@@ -0,0 +1,24 @@
+console.clear();
+
+let num = 10;
+console.log(num);
+
+let name = 'Jojn';
+console.log(name);
+
+const API_URL = 'cdasdc';
+console.log(API_URL);
+
+const GREETINGS = `Hello ${name}`;
+console.log(GREETINGS);
+
+const mGreetings = (name) => {
+    return `Welcome ${name}`
+}
+
+const mGreetings1 = name => {
+    return `Welcome ${name}`
+}
+
+console.log(mGreetings('Tan'))
+console.log(mGreetings1('Tan'))
